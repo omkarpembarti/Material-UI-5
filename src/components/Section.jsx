2382e5d09e511c142f3a0002a2e66bfdca50d34d
OmkarPembarti <omkarpembarti@gmail.com>
2023-06-25 18:09:11 +0530

    random

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Paper, Typography } from "@mui/material";
+import { Box, Container, Divider, Paper, Typography } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import {
@@ -10,72 +10,78 @@ import { useEffect, useState } from "react";
 const Section = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+    setTimeout(() => {
+      fetch("https://fakestoreapi.com/products")
+        .then((response) => response.json())
+        .then((data) => setProducts(data));
+    }, 3000);
     console.log(products);
   }, []);
 
   return (
     <>
-      <Divider variant="fullWidth"></Divider>
-      <Typography variant="h5">I am section</Typography>
-      <Box
+      <Container
+        maxWidth="lg"
         sx={{
           display: "flex",
           flexDirection: "row",
           gap: "5px",
-          //   backgroundColor: "lightgray",
+          backgroundColor: "lightgray",
           flexWrap: "wrap",
+          justifyContent: "space-evenly",
         }}>
-        {products.map((product) => {
-          return (
-            <Paper
-              key={product.id}
-              elevation={10}
-              variant="elevation"
-              square={false}
-              sx={{
-                maxWidth: 345,
-              }}>
-              <CardActionArea disableRipple>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {product.title.split(" ")[0]}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    component="div"
-                    sx={{
-                      height: 100, // Set the desired height for the container
-                      overflow: "auto", // Enable scrolling
-                    }}>
-                    {product.description}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button
-                  startIcon={<AddShoppingCartOutlined />}
-                  size="small"
-                  variant="contained"
-                  color="success"
-                  sx={{ marginLeft: "auto" }}>
-                  Add
-                </Button>
-                <Button
-                  startIcon={<RemoveShoppingCart />}
-                  size="small"
-                  variant="contained"
-                  color="error">
-                  Remove
-                </Button>
-              </CardActions>
-            </Paper>
-          );
-        })}
-      </Box>
+        {products.length == 0 ? (
+          <h2>Loading...</h2>
+        ) : (
+          products.map((product) => {
+            return (
+              <Paper
+                key={product.id}
+                elevation={10}
+                variant="elevation"
+                square={false}
+                sx={{
+                  maxWidth: 345,
+                }}>
+                <CardActionArea disableRipple>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {product.title.split(" ")[0]}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      component="div"
+                      sx={{
+                        height: 100, // Set the desired height for the container
+                        overflow: "auto", // Enable scrolling
+                      }}>
+                      {product.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    startIcon={<AddShoppingCartOutlined />}
+                    size="small"
+                    variant="contained"
+                    color="success"
+                    sx={{ marginLeft: "auto" }}>
+                    Add
+                  </Button>
+                  <Button
+                    startIcon={<RemoveShoppingCart />}
+                    size="small"
+                    variant="contained"
+                    color="error">
+                    Remove
+                  </Button>
+                </CardActions>
+              </Paper>
+            );
+          })
+        )}
+      </Container>
     </>
   );
 };
