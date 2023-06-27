1a08603130d4c4164a692111cb5ae598d0a39a6d
OmkarPembarti <omkarpembarti@gmail.com>
2023-06-27 22:50:13 +0530

    Added responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,64 @@
 /* eslint-disable react/prop-types */
-import { LogoDev } from "@mui/icons-material";
-import { AppBar, Button, Switch, Toolbar, Typography } from "@mui/material";
+import { LogoDev, LoginSharp } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Switch,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import { Fragment } from "react";
 import TabComponent from "./TabComponent";
+import { useTheme } from "@emotion/react";
 
 const Header = (props) => {
+  const theme = useTheme();
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
   return (
     <Fragment>
       <AppBar
         position="sticky"
-        sx={{ backgroundColor: `${props.mode ? "" : "#d68c45"}` }}>
+        sx={{
+          backgroundColor: props.mode ? "" : "transparent",
+          backgroundImage: props.mode
+            ? "none"
+            : "linear-gradient(to left, #003399 0%, #ff0000 100%)",
+        }}>
         <Toolbar>
-          <LogoDev sx={{ fontSize: "53px" }} />
-          <Typography variant="h5">Nilam</Typography>
+          <LogoDev
+            sx={{
+              fontSize: "53px",
+            }}
+          />
+          <Typography variant="h5">Project</Typography>
           <TabComponent />
-
-          <Button
-            variant="contained"
-            color="success"
-            sx={{ marginLeft: "auto" }}
-            disableRipple
-            disableElevation>
-            Sign-In
-          </Button>
-          <Button variant="contained" color="error" sx={{ marginLeft: "5px" }}>
-            Log-In
-          </Button>
+          <Box sx={{ marginLeft: "auto" }}>
+            {isMediumScreen || (
+              <>
+                <Button
+                  variant="contained"
+                  color="success"
+                  disableRipple
+                  disableElevation>
+                  Sign-In
+                </Button>
+                <Button
+                  variant="contained"
+                  color="error"
+                  sx={{ marginLeft: "5px" }}>
+                  Log-In
+                </Button>
+              </>
+            )}
+            {isMediumScreen && (
+              <IconButton>
+                <LoginSharp color="success" />
+              </IconButton>
+            )}
+          </Box>
           <Switch
             checked={props.mode}
             color="warning"
--- src/components/TabComponent.jsx
+++ src/components/TabComponent.jsx
@@ -1,15 +1,20 @@
 // Tabs index start with 0 index
 
-import { Tab, Tabs } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { Tab, Tabs, useMediaQuery } from "@mui/material";
 import { useState } from "react";
 
 const TabComponent = () => {
   const [value, setValue] = useState(0);
+
+  const theme = useTheme();
+  const isXsScreen = useMediaQuery(theme.breakpoints.up("sm"));
+
   return (
     <>
       <Tabs
         value={value}
-        sx={{ margin: "auto" }}
+        sx={{ margin: "auto", display: isXsScreen ? "block" : "none" }}
         textColor="inherit"
         indicatorColor="primary"
         onChange={(e, value) => {
